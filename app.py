095e232a1610b63e4b144d3915e44b8f03476cfa
nauman-tintash <nauman@tintash.com>
2018-06-12 21:03:21 +0500

    Added some more comments for debugging.

--- app.py
+++ app.py
@@ -13,30 +13,31 @@ app = Flask(__name__)
 #We will receive messages that Facebook sends our bot at this endpoint 
 @app.route('/', methods=['GET', 'POST'])
 def receive_message():
+    print('Receiving Message')
     if request.method == 'GET':
+        print('Method : GET')
     	# Before allowing people to message your bot, Facebook has implemented a verify token
     	# that confirms all requests that your bot receives came from Facebook.
-        print ('Got Message')
-        print (request.args)
         token_sent = request.args.get("hub.verify_token")
         return verify_fb_token(token_sent)
     #if the request was not get, it must be POST and we can just proceed with sending a message back to user
     else:
-       # get whatever message a user sent the bot
-       output = request.get_json()
-       for event in output['entry']:
-          messaging = event['messaging']
-          for message in messaging:
-            if message.get('message'):
-                #Facebook Messenger ID for user so we know where to send response back to
-                recipient_id = message['sender']['id']
-                if message['message'].get('text'):
-                    response_sent_text = get_message()
-                    send_message(recipient_id, response_sent_text)
-                #if user sends us a GIF, photo,video, or any other non-text item
-                if message['message'].get('attachments'):
-                    response_sent_nontext = get_message()
-                    send_message(recipient_id, response_sent_nontext)
+        print('Method : Post')
+        # get whatever message a user sent the bot
+        output = request.get_json()
+        for event in output['entry']:
+            messaging = event['messaging']
+            for message in messaging:
+                if message.get('message'):
+                    #Facebook Messenger ID for user so we know where to send response back to
+                    recipient_id = message['sender']['id']
+                    if message['message'].get('text'):
+                        response_sent_text = get_message()
+                        send_message(recipient_id, response_sent_text)
+                    #if user sends us a GIF, photo,video, or any other non-text item
+                    if message['message'].get('attachments'):
+                        response_sent_nontext = get_message()
+                        send_message(recipient_id, response_sent_nontext)
     return "Message Processed"
 
 def verify_fb_token(token_sent):

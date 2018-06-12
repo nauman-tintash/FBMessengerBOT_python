78788e873f8eb3462139b691a08c4a6622c2a501
nauman-tintash <nauman@tintash.com>
2018-06-12 20:49:54 +0500

    added some log to debug verification function.

--- app.py
+++ app.py
@@ -15,9 +15,11 @@ app = Flask(__name__)
 def receive_message():
     if request.method == 'GET':
     	# Before allowing people to message your bot, Facebook has implemented a verify token
-    	# that confirms all requests that your bot receives came from Facebook. 
-    	token_sent = request.args.get("hub.verify_token")
-    	return verify_fb_token(token_sent)
+    	# that confirms all requests that your bot receives came from Facebook.
+        print ('Got Message')
+        print (request.args)
+        token_sent = request.args.get("hub.verify_token")
+        return verify_fb_token(token_sent)
     #if the request was not get, it must be POST and we can just proceed with sending a message back to user
     else:
        # get whatever message a user sent the bot
